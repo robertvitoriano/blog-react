2ba7b547e0089def4f940b2ede52706f126740ed
robertvitoriano <robertvitorianodev@gmail.com>
2020-09-15 17:05:09 -0300

    creating styles colors

--- src/components/common/mansory-post.js
+++ src/components/common/mansory-post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import categoryColors from './styles'
 
 function MansoryPost({post,tagsOnTop}){
     const style  = {backgroundImage:`url("${require(`../../assets/img/${post.image}`)}")`} 
@@ -6,9 +7,16 @@ function MansoryPost({post,tagsOnTop}){
     return(
         <a className="mansory-post overlay" style={style} href={post.link}>
             <div className="image-text">
-
+             <div className="tags-container">
+                 {post.categories.map((tag,index)=>
+                 <span key={index} className="tag" style={{backgroundColor:categoryColors[tag]}}>
+                     {tag.toUpperCase()}
+                 </span>)}
+             </div>
                 <div>
-                    <h className="image-title">{post.title}</h>
+                    <h2 className="image-title">{post.title}</h2>
+    <span className="iamge-date">{post.date}</span>
+
                 </div>
             </div>
         </a>
